96e9da65968bddd7ef284a256e7898ac8d1ebfe6
rhamill1 <rhamill11@gmail.com>
2017-01-07 15:24:06 -0800

    adds opacity on scroll to title-box

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 console.log('app.js is loaded');
 
 $(document).ready(function(){
-
   $(window).stellar();
-
 })
+
+$(window).scroll(function(){
+  var fromTop = $(document).scrollTop();
+  var opacityValue = (100 - (fromTop * .3)) / 100
+  $('.title-box').css({
+    opacity: opacityValue
+  });
+});
